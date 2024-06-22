ae5e356fb2a9524404d9586c1c70b267e920c651
kh-jannatul <khjannatulferdous16@gmail.com>
2024-06-22 11:30:13 +0600

    Add files via upload

---
+++ 1006.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a,b,c,avarage;
+    cin>>a>>b>>c;
+    avarage=(a*2.0+b*3.0+c*5.0)/10.0;
+    cout<<"MEDIA = "<<fixed<<setprecision(1)<<avarage<<endl;
+    return 0;
+}
+
+
+
