538459882ba8ed530a2dde848357f9fd0cdff5e3
kh-jannatul <khjannatulferdous16@gmail.com>
2024-06-27 10:20:08 +0600

    27/06/24

---
+++ 1009.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string name;
+    double a,b;
+    cin>>name;
+    cin>>a;
+    cin>>b;
+    cout<<fixed<<setprecision(2)<<"TOTAL = R$ "<<(b*0.15)+a<<endl;
+    return 0;
+}
+
---
+++ 1011.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double r;
+    cin>>r;
+    cout<<fixed<<setprecision(3)<<"VOLUME = "<<(4.0/3) * 3.14159 * r*r*r<<endl;
+    return 0;
+}
